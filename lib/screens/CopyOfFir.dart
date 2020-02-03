9aab53c08550a5da090e55e26d56f53701b14338
rt2939 <aggarwal.ram24@gmail.com>
2020-02-04 00:32:07 +0530

    Updates

--- lib/screens/CopyOfFir.dart
+++ lib/screens/CopyOfFir.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
+class CopyOfFir extends StatefulWidget {
+  String fir;
+  CopyOfFir(this.fir);
+  @override
+  _CopyOfFirState createState() => _CopyOfFirState(fir);
+}
 
-class CopyOfFir extends StatelessWidget {
+class _CopyOfFirState extends State<CopyOfFir> {
+String fir;
+
+  TextStyle style=TextStyle(
+    fontSize: 50,
+  );
+  var database=FirebaseDatabase.instance.reference().child("Users");
+
+_CopyOfFirState(this.fir);
+  Map<dynamic,dynamic> record={
+      'name':"Name",
+      'date':"Date",
+      'age' :'Age',
+      'height':'height'
+    };
+
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
@@ -174,20 +197,25 @@ class CopyOfFir extends StatelessWidget {
     ));
   }
 
-  Widget fields(String x) {
-    return Padding(
-        padding: EdgeInsets.only(left: 10, right: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Expanded(
-                child: Text(
-              x,
-              textAlign: TextAlign.center,
-              textScaleFactor: 1.2,
-            )),
-            Expanded(child: TextFormField()),
-          ],
-        ));
+  Future<Map<dynamic,dynamic>> getUsers() async {
+    Completer<Map<dynamic,dynamic>> l = new Completer<Map<dynamic,dynamic>>();
+    Map<dynamic,dynamic> x = {};
+    database.child(fir)
+        .once()
+        .then((DataSnapshot snapshot) {
+      final value = snapshot.value as Map;
+      x['name']=value['name'];
+      x['date']=value['date'];
+      x['age']=value['age'];
+      x['height']=value['height'];
+      l.complete(x);
+      });
+    return l.future;
+  }
+
+  update(Map<dynamic,dynamic> value) {
+    setState(() {
+      record = value;
+    });
   }
 }
